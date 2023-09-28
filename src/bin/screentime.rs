e220fd3ec00a1dfe9e87281625b39b224cfed11c
klaxa <klaxa1337@googlemail.com>
2023-09-28 03:35:17 +0200

    Initial commit

---
+++ src/bin/screentime.rs
@@ -0,0 +1,69 @@
+use rusqlite::Connection;
+use chrono::prelude::*;
+use chrono::naive::Days;
+use chrono::{Datelike, Local, Duration};
+use std::env;
+
+#[derive(Debug)]
+struct Res {
+    class: String,
+    count: i64,
+}
+
+fn min(a: usize, b: usize) -> usize {
+    if a < b {
+        return a;
+    }
+    b
+}
+
+fn fmt(ts: Duration) -> String {
+    format!("{}:{:0>2}:{:0>2} ", ts.num_hours(), ts.num_minutes() % 60, ts.num_seconds() % 60)
+}
+
+fn main() {
+    let mut db = if let Ok(s) = env::var("TRACKING_DB") {
+        s
+    } else {
+        "tracking.db".to_string()
+    };
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        db = args[1].clone();
+    }
+    let conn = Connection::open(&db).unwrap();
+    let now = Local::now();
+    let zero_hour = Local.with_ymd_and_hms(now.year(), now.month(), now.day(), 0, 0, 0).unwrap();
+    let twenty_fourth_hour = zero_hour.checked_add_days(Days::new(1)).unwrap();
+    let query = &format!("select class, count (*) FROM tracking where ts > {} and ts < {} group by class;", zero_hour.timestamp(), twenty_fourth_hour.timestamp());
+    let mut stmt = conn.prepare(query).unwrap();
+    
+    let res = stmt.query_map((), |row| {
+        Ok(Res {
+            class: row.get(0)?,
+            count: row.get(1)?,
+        })
+    }).unwrap();
+    //let count: i64 = conn.query_row(query, [], |row| row.get(0)).unwrap();
+    
+    
+    let mut counts = vec![];
+    let mut count = 0;
+    for r in res {
+        let c = r.unwrap();
+        count += c.count;
+        if c.class != "feh" {
+            counts.push(c);
+        }
+    }
+    
+    counts.sort_by(|a, b| b.count.partial_cmp(&a.count).unwrap());
+    let nb = min(counts.len(), 3);
+    let mut output = fmt(Duration::seconds(count * 10));
+    for i in 0..nb {
+        output += counts[i].class.as_str();
+        output += ": ";
+        output += &fmt(Duration::seconds(counts[i].count * 10));
+    }
+    println!("{}", output);
+}
---
+++ src/bin/tracking.rs
@@ -0,0 +1,84 @@
+use rusqlite::Connection;
+use std::env;
+use std::thread::sleep;
+use std::time::Duration;
+use std::process::Command;
+use json;
+use chrono::Local;
+use lazy_static::lazy_static;
+
+#[derive(Debug)]
+struct FocusEntry {
+    class: String,
+    title: String,
+    ts: String,
+}
+
+lazy_static! {
+    static ref EMPTY: json::JsonValue = json::object!{};
+}
+
+fn get_focused_window(obj: &json::JsonValue) -> &json::JsonValue {
+    if obj["focused"] == true {
+        return obj;
+    }
+    if !obj["nodes"].is_null() {
+        for node in obj["nodes"].members() {
+            let res = get_focused_window(node);
+            if res["focused"] == true {
+                return res;
+            }
+        }
+    }
+    
+    return &EMPTY;
+}
+
+fn main() {
+    let mut db = if let Ok(s) = env::var("TRACKING_DB") {
+        s
+    } else {
+        "tracking.db".to_string()
+    };
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        db = args[1].clone();
+    }
+    let conn = Connection::open(&db).unwrap();
+    conn.execute(
+        "create table if not exists tracking (
+            id integer primary key,
+            class text not null,
+            title text not null,
+            ts integer not null
+    );",
+    (),
+    ).unwrap();
+    loop {
+        
+        let output = Command::new("i3-msg").args(["-t", "get_tree"]).output().expect("Could not call i3-msg -t get_tree");
+        let output = String::from_utf8(output.stdout).unwrap_or_default();
+        let output = json::parse(&output).unwrap_or(json::JsonValue::Null);
+        let focus = get_focused_window(&output);
+        
+        let now = Local::now().timestamp().to_string();
+        let focus_entry = if focus["window_properties"].is_null() {
+            FocusEntry {
+                class: "idle".to_string(),
+                title: "idle".to_string(),
+                ts: now
+            }
+        } else {
+            FocusEntry {
+                class: focus["window_properties"]["class"].to_string(),
+                title: focus["window_properties"]["title"].to_string(),
+                ts: now
+            }
+        };
+        conn.execute(
+            "INSERT INTO tracking (class, title, ts) values (?1, ?2, ?3);",
+                    &[&focus_entry.class, &focus_entry.title, &focus_entry.ts],
+        ).unwrap();
+        sleep(Duration::from_secs(10));
+    }
+}
